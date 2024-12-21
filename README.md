4df6504561d735b984aded7d662a1b6389c90204
jgy147 <jgy17563226696@qq.com>
2024-12-21 12:50:24 +0800

    Updata

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for readboy Readboy_C6Pro (tb8788p1_64_wifi)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_tb8788p1_64_wifi-user
+add_lunch_combo omni_tb8788p1_64_wifi-userdebug
+add_lunch_combo omni_tb8788p1_64_wifi-eng
